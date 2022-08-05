881064367f02202420ab89e0f4557046b97bffa4
Jonathan Arnett <jonarnett90@gmail.com>
2022-08-05 12:12:44 -0400

    First pass

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,17 @@
-use clap::Parser;
+#![feature(absolute_path)]
+
 use std::io;
-use std::path::Path;
+use std::{path, path::Path};
+
+use askama::Template;
+use clap::Parser;
 use serde::Serialize;
+
+#[derive(Template)]
+#[template(path = "tree.html", escape = "none")]
+struct TreeTemplate {
+    tree_data: String,
+}
 
 #[derive(Parser, Debug)]
 #[clap(version, author, about)]
@@ -19,16 +29,16 @@ struct Node {
 
 fn main() -> io::Result<()> {
     let args = Args::parse();
+    let tree_data =
+        serde_json::to_string(&process_dir(&Path::new(&args.directory)).unwrap()).unwrap();
 
-    println!("{:?}", serde_json::to_string(&process_dir(&Path::new(&args.directory)).unwrap()));
-
+    println!("{}", generate_output(tree_data));
     Ok(())
 }
 
-fn process_dir(dirname: &Path) -> io::Result<Node> {
-    let children = if dirname.is_dir() {
-        dirname
-            .read_dir()?
+fn process_dir(dir: &Path) -> io::Result<Node> {
+    let children = if dir.is_dir() {
+        dir.read_dir()?
             .flatten()
             .map(|file| process_dir(&file.path()))
             .flatten()
@@ -37,8 +47,15 @@ fn process_dir(dirname: &Path) -> io::Result<Node> {
         vec![]
     };
 
-    Ok(Node {
-        name: dirname.file_name().unwrap().to_str().unwrap().to_string(),
-        children,
-    })
+    let name = match dir.file_name() {
+        Some(name) => name.to_str().unwrap().to_string(),
+        None => String::from("."),
+    };
+
+    Ok(Node { name, children })
+}
+
+fn generate_output(tree_data: String) -> String {
+    let template = TreeTemplate { tree_data };
+    template.render().unwrap()
 }
